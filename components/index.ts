fc12ec5d20fbcd21b56b6be170446dd7d4b39c0c
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-19 13:24:27 +0400

    Added home components

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,8 @@
 export { default as NearbyJobCard } from './common/cards/nearby/NearbyJobCard';
 export { default as PopularJobCard } from './common/cards/popular/PopularJobCard';
 export { default as ScreenHeaderBtn } from './common/header/ScreenHeaderBtn';
+
+// Home
+export { default as Welcome } from './home/welcome/Welcome';
+export { default as NearbyJobs } from './home/nearby/NearbyJobs';
+export { default as PopularJobs } from './home/popular/PopularJobs';
