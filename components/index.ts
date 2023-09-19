bfd3b210c2c3d3ff8a8449cb709b92ea92ac3417
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-19 13:39:21 +0400

    Added components of job details

--- components/index.ts
+++ components/index.ts
@@ -7,3 +7,10 @@ export { default as ScreenHeaderBtn } from './common/header/ScreenHeaderBtn';
 export { default as Welcome } from './home/welcome/Welcome';
 export { default as NearbyJobs } from './home/nearby/NearbyJobs';
 export { default as PopularJobs } from './home/popular/PopularJobs';
+
+// Job Details
+export { default as Tabs } from './jobdetails/tabs/Tabs';
+export { default as About } from './jobdetails/about/About';
+export { default as Footer } from './jobdetails/footer/Footer';
+export { default as Company } from './jobdetails/company/Company';
+export { default as Specifics } from './jobdetails/specifics/Specifics';
---
+++ components/jobdetails/tabs/Tabs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const Tabs = () => {
+  return (
+    <View>
+      <Text>Tabs</Text>
+    </View>
+  )
+}
+
+export default Tabs;
