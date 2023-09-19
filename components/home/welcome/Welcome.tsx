67c56f69c710632a4a18de035e7483e23ff95303
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-19 19:55:03 +0400

    Added search input and button

--- components/home/welcome/Welcome.tsx
+++ components/home/welcome/Welcome.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { icons } from '../../../constants';
 import styles from './styles';
 
 const Welcome = () => {
+  const [search, setSearch] = useState<string>('');
+  
   return (
-    <View>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <View>
         <Text style={styles.userName}>Hello Giga</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            placeholder='What are you looking for?'
+            value={search}
+            onChangeText={(value) => {setSearch(value)}}
+            style={styles.searchInput}
+          />
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => {}}
+          style={styles.searchBtn}
+        >
+          <Image 
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   )
--- components/home/welcome/styles.js
+++ components/home/welcome/styles.js
@@ -4,6 +4,7 @@ import { COLORS, FONT, SIZES } from '../../../constants';
 const styles = StyleSheet.create({
   container: {
     width: '100%',
+    marginTop: SIZES.large,
   },
   userName: {
     fontFamily: FONT.regular,
