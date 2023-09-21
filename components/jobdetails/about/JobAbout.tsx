1996d8de57eac013187a47979230756c62496d3d
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-21 11:18:47 +0400

    Added content for JobAbout component

--- components/jobdetails/about/JobAbout.tsx
+++ components/jobdetails/about/JobAbout.tsx
@@ -5,8 +5,11 @@ import styles from './styles';
 
 const JobAbout = ({ info }: IJobAbout) => {
   return (
-    <View>
-      <Text>JobAbout</Text>
+    <View style={styles.container}>
+      <Text style={styles.headText}>About the job:</Text>
+      <View style={styles.contentBox}>
+        <Text style={styles.contextText}>{info}</Text>
+      </View>
     </View>
   )
 }
